a9ba2df661d0b50efcead4ccfcc60692f5b522af
chris-schmitz <schmitz.chris@gmail.com>
2018-11-17 09:35:23 -0600

    Moves code to a subfolder and adds lib directory files

--- code/lib/hoodie.py
+++ code/lib/hoodie.py
@@ -1,17 +1,4 @@
-from touchio import TouchIn
-import adafruit_dotstar as dotstar
 import time
-import board
-import neopixel
-
-
-jewel = neopixel.NeoPixel(board.A1, 7, brightness=0.1, auto_write=True)
-dot = dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1, brightness=0.2)
-
-touchL = TouchIn(board.A0)
-touchR = TouchIn(board.A2)
-touchL.threshold = touchL.raw_value + 200
-touchR.threshold = touchR.raw_value + 200
 
 
 class Hoodie:
@@ -66,26 +53,3 @@ class Hoodie:
                 self.jewel[self.count] = self.colorPositions[0]
 
         time.sleep(.1)
-
-
-hoodie = Hoodie(jewel)
-
-while True:
-    # print(touchL.raw_value)
-    if touchL.value and touchR.value:
-        print("touched both")
-        jewel.fill((255, 255, 255))
-
-    elif touchL.value:
-        print("touched left")
-        dot[0] = [0, 255, 0]
-        hoodie.handleColor('wheel')
-
-    elif touchR.value:
-        print("touched right")
-        dot[0] = [0, 255, 0]
-        hoodie.handleColor('extend')
-
-    else:
-        dot[0] = [255, 0, 0]
-        jewel.fill((255, 0, 0))
---
+++ code/main.py
@@ -0,0 +1,38 @@
+from touchio import TouchIn
+import adafruit_dotstar as dotstar
+import board
+import neopixel
+from hoodie import Hoodie
+
+
+jewel = neopixel.NeoPixel(board.A1, 7, brightness=0.1, auto_write=True)
+dot = dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1, brightness=0.2)
+
+touchL = TouchIn(board.A0)
+touchR = TouchIn(board.A2)
+touchL.threshold = touchL.raw_value + 200
+touchR.threshold = touchR.raw_value + 200
+
+
+hoodie = Hoodie(jewel)
+
+while True:
+    # print(touchL.raw_value)
+
+    if touchL.value and touchR.value:
+        print("touched both")
+        jewel.fill((255, 255, 255))
+
+    elif touchL.value:
+        print("touched left")
+        dot[0] = [0, 255, 0]
+        hoodie.handleColor('wheel')
+
+    elif touchR.value:
+        print("touched right")
+        dot[0] = [0, 255, 0]
+        hoodie.handleColor('extend')
+
+    else:
+        dot[0] = [255, 0, 0]
+        jewel.fill((255, 0, 0))
